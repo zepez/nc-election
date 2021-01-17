5e375ce080c26156cddd480d995f0f65527c862d
Alex Zepezauer <zepezaueralex@gmail.com>
2021-01-16 21:13:17 -0500

    add support for env

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import asyncio
 from pyppeteer import launch
 import scraper
+import os
+import json
 
+os.environ['pages'] = '["https://er.ncsbe.gov/?election_dt=11/03/2020&county_id=0&office=FED&contest=0", "https://er.ncsbe.gov/?election_dt=11/03/2020&county_id=0&office=CCL&contest=0"]'
+os.environ['endpoint'] = ''
+os.environ['cron'] = '* * * * *'
 
 
 async def main():
     #launch browser
-    browser = await launch({"headless": True})
-    # new page and go to location
-    page = await browser.newPage()
-    await page.goto('https://er.ncsbe.gov/?election_dt=11/03/2020&county_id=0&office=FED&contest=0')
+    browser = await launch({"headless": False})
+    
+    for page_to_scrape in json.loads(os.environ.get('pages')):
+        # new page and go to location
+        page = await browser.newPage()
+        await page.goto(page_to_scrape)
 
-	# get the entire page for parsing
-    response = await page.evaluate('''() => {
-        return document.documentElement.innerHTML
-    }''')
+        # get the entire page for parsing
+        response = await page.evaluate('''() => {
+            return document.documentElement.innerHTML
+        }''')
 
-	# call scrape data
-    scraper.scrape(response)
+        # call scrape data
+        scraper.scrape(response)
+        
+    
 
 	# close the broswer
     await browser.close()
--- sender.py
+++ sender.py
@@ -1,6 +1,8 @@
 import requests
+import os
+
 
 
 def send(json):
-  x = requests.post("http://localhost:3001/rail/test", data = json, headers = {'Content-Type': 'application/json', 'Accept-Encoding': 'UTF-8'})
+  x = requests.post(os.environ.get('endpoint'), data = json, headers = {'Content-Type': 'application/json', 'Accept-Encoding': 'UTF-8'})
   print(x.status_code)
